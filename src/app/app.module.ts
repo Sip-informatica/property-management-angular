774178bf68e1a148ac302b11ceb2b726bb661c79
Candi <open.hardware.libre@gmail.com>
2021-02-27 20:04:47 +0000

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './property-management/board-admin/board-admin.component';
+import { BoardManagementComponent } from './property-management/board-management/board-management.component';
+import { BoardUserComponent } from './property-management/board-user/board-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardManagementComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
