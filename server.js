6a6b40a1b81e863b757eb003c5695125dbbf988b
Candi <open.hardware.libre@gmail.com>
2021-03-01 10:37:37 +0000

    Herohu config

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const PATH_PROJECT = '/dist/sip-property-management-angula';
-import express, { static } from 'express';
+const express = require('express');
 const app = express();
-import { join } from 'path';
+const path = require('path');
 
-app.use(static(__dirname + PATH_PROJECT));
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname + PATH_PROJECT + '/index.html'));
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
+var server_port = process.env.PORT || 8080;
+var server_host = '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
-
-app.listen(process.env.PORT || 5000);
