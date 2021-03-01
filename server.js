c8d8c472a0c9381f9b262053eb3a3275b79036d3
Candi <open.hardware.libre@gmail.com>
2021-03-01 11:52:39 +0000

    Heroku config

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + PATH_PROJECT));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + PATH_PROJECT + '/index.html'));
   });
 
 var server_port = process.env.PORT || 8080;
