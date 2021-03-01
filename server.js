21ce6e03abbd2ae5df70c3b1ba41747d423d5282
Candi <open.hardware.libre@gmail.com>
2021-03-01 13:49:58 +0000

    Heroku config

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const PATH_PROJECT = '/dist/sip-property-management-angula';
 const express = require('express');
 const app = express();
+
+const http = require('http');
 const path = require('path');
+const server = http.createServer(app);
 
 app.use(express.static(__dirname + PATH_PROJECT));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + PATH_PROJECT + '/index.html'));
-  });
-
-var server_port = process.env.PORT || 8080;
-var server_host = '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+server.listen(process.env.PORT || 8080, () => {
+  if (!process.env.PORT) {
+    console.log('Running with Express... http://localhost:8080/');
+  }
 });
