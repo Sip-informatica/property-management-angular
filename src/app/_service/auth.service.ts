7e29f95893825174835b451a0093df3b335602d4
Candi <open.hardware.libre@gmail.com>
2021-03-03 19:41:00 +0000

    Crear servicio de autenticación

--- src/app/_service/auth.service.ts
+++ src/app/_service/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import {environment} from '@env';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(environment.AUTH_API + 'signin', {
+      username,
+      password
+    }, httpOptions);
+
+  }
+
+  register(username: string, email: string, password: string): Observable<any>{
+    return this.http.post(environment.AUTH_API + 'signup', {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }
 }
