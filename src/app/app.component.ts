0b49e2a3ac7ca26a37c76799fda64570a1b52710
Candi <open.hardware.libre@gmail.com>
2021-03-05 17:22:12 +0000

    Componente aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '@core/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'property-management-angular';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showManagementBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showManagementBoard = this.roles.includes('ROLE_MANAGEMENT');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
