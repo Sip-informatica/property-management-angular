563f8bc8ec3199f2093cdde1c19aa88c1277638e
Candido Garcia Romero <candidogarcia@santaluciagc.com>
2021-03-11 14:07:08 +0000

    configuration primeng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { BoardUserComponent } from './property-management/board-user/board-user.
 
 const routes: Routes = [
   { path: 'home',  loadChildren: () => import('./home/home.module').then(module => module.HomeModule) },
-  { path: 'primeng',  loadChildren: () => import('./primeng/primeng.module').then(module => module.PrimengModule) },
+  { path: 'shared',  loadChildren: () => import('./shared/primeng.module').then(module => module.PrimengModule) },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
