42e162f7ad4387b8bd6cfe9855a770e57f41c96e
Candi <open.hardware.libre@gmail.com>
2021-03-13 16:41:31 +0000

    menubar

--- src/app/shared/primeng-routing.module.ts
+++ src/app/shared/primeng-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MenubarComponent } from './component/menubar/menubar.component';
+
+const routes: Routes = [
+  { path: 'menubar', component: MenubarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
