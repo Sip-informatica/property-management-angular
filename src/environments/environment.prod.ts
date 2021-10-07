914ead395aa28ca9169be177fb5115da91f36d99
Candido Garcia Romero <candidogarcia@santaluciagc.com>
2021-10-07 10:23:05 +0100

    dialog create board admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   AUTH_API: 'https://sip-property-management.herokuapp.com/api/auth/',
-  API_URL_TEST: 'https://sip-property-management.herokuapp.com/api/test/'
+  API_URL_TEST: 'https://sip-property-management.herokuapp.com/api/test/',
+  ADMIN_API: 'https://sip-property-management.herokuapp.com/api/admin/'
 };
