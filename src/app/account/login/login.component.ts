f66b682d174e6758c27f6f30597910e85c87e77e
Candido Garcia Romero <candidogarcia@santaluciagc.com>
2021-10-19 14:08:38 +0100

    loading register

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@core/_service/auth.service';
 import { TokenStorageService } from '@core/_service/token-storage.service';
+import { timeout } from 'rxjs/operators';
+import { Logs } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   form: any = {
     username: null,
-    password: null
+    password: null,
   };
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
+  loading = false;
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) {}
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -27,23 +33,27 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.loading = true;
     const { username, password } = this.form;
 
-    this.authService.login(username, password).subscribe(
-      data => {
-        this.tokenStorage.saveToken(data.token);
-        this.tokenStorage.saveUser(data);
-
-        this.isLoginFailed = false;
-        this.isLoggedIn = true;
-        this.roles = this.tokenStorage.getUser().roles;
-        this.reloadPage();
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isLoginFailed = true;
-      }
-    );
+    this.authService
+      .login(username, password)
+      .pipe(timeout(1000))
+      .subscribe(
+        (data) => {
+          this.tokenStorage.saveToken(data.token);
+          this.tokenStorage.saveUser(data);
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+          this.roles = this.tokenStorage.getUser().roles;
+          this.reloadPage();
+        },
+        (err) => {
+          this.errorMessage = err.error.message;
+          this.isLoginFailed = true;
+          this.loading = false;
+        }
+      );
   }
   reloadPage(): void {
     window.location.assign('#');
